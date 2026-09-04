188b1c23939d7b2dce2b732d308c73012518670e
flpzht <flpcsb@hotmail.com>
2026-09-04 10:52:56 -0300

    feat: adiciona marcação do Header e Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
+import Navigation from '@/components/Navigation/Navigation';
+
 import '@/components/Header/Header.css';
 
 function Header() {
   return (
     <header className="header">
+      <p className="header__logo">News Explorer</p>
+      <Navigation />
     </header>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,29 @@
+import { Link, useLocation } from 'react-router-dom';
+
 import '@/components/Navigation/Navigation.css';
 
 function Navigation() {
+  const location = useLocation();
+
   return (
     <nav className="navigation">
+      <Link
+        to="/"
+        className={`navigation__link ${location.pathname === '/' ? 'navigation__link_active' : ''}`}
+      >
+        Início
+      </Link>
+
+      <Link
+        to="/saved-news"
+        className={`navigation__link ${location.pathname === '/saved-news' ? 'navigation__link_active' : ''}`}
+      >
+        Artigos salvos
+      </Link>
+
+      <button className="navigation-signin" type='button'>Entrar</button>
+
+      <button className="navigation__signout" type="button">Usuário</button>
     </nav>
   );
 }
