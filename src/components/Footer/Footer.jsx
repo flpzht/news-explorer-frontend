04e7b5e331096d9ac172dd5eb51902fa5f9f7cff
flpzht <flpcsb@hotmail.com>
2026-09-04 11:37:58 -0300

    feat: adiciona marcação do Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,26 @@
+import { Link } from 'react-router-dom';
+
 import '@/components/Footer/Footer.css';
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <footer className="footer">    
+        <footer className="footer">
+            <p>&copy; {currentYear} Supersite, desenvolvido pela News API</p>
+
+            <div className="footer__links">
+                <Link to="/" className="footer__link">Início</Link>
+                <a href="https://tripleten.com.br/" className="footer__link" target="_blank" rel="noopener noreferrer">
+                    Tripleten
+                </a>
+                <a href="https://github.com/flpzht/news-explorer-frontend" className="footer__icon-link footer__icon-link_type-github" target="_blank" rel="noopener noreferrer" aria-label='GitHub'>
+                    {/* Ícone direto no style */}
+                </a>
+                <a href="https://www.linkedin.com/in/felipecarvalhodesouzabarros/" className="footer__icon-link footer__icon-link_type-linkedin" target="_blank" rel="noopener noreferrer" aria-label='LinkedIn'>
+                    {/* Ícone direto no style */}
+                </a>
+            </div>
         </footer>
     );
 }
