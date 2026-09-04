60ddf9820259c8a7c3506ce1486306d25b79f31d
flpzht <flpcsb@hotmail.com>
2026-09-04 01:06:18 -0300

    feat: configura rotas e composição do App com Header e Footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from '@/components/Header/Header.jsx';
+import Main from '@/components/Main/Main.jsx';
+import SavedNews from '@/components/SavedNews/SavedNews.jsx';
+import Footer from '@/components/Footer/Footer.jsx';
+
 import '@/components/App/App.css'
 
 function App() {
 
   return (
     <div className="app">
-      <h1 className="app__title">News Explorer</h1>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/saved-news" element={<SavedNews />} />
+        </Routes>
+      <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
