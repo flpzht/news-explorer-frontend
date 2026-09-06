8c243ca215462eb7b58afe082c880912e09e536f
flpzht <flpcsb@hotmail.com>
2026-09-06 19:28:38 -0300

    feat: adiciona marcação do PopupWithForm com modo login/registro

--- src/components/PopupWithForm/PopupWithForm.jsx
+++ src/components/PopupWithForm/PopupWithForm.jsx
@@ -1,8 +1,33 @@
 import '@/components/PopupWithForm/PopupWithForm.css';
 
-function PopupWithForm() {
+function PopupWithForm({ title, buttonText, switchText, switchLinkText, children }) {
     return (
         <div className="popup-with-form">
+            <div className="popup-with-form__container">
+                <button
+                    className="popup-with-form__close-button"
+                    type="button"
+                    aria-label="Fechar"
+                >
+                </button>
+
+                <h2 className="popup-with-form__title">{title}</h2>
+
+                <form className="popup-with-form__form" noValidate>
+                    {children}
+
+                    <button className="popup-with-form__submit" type="submit">
+                        {buttonText}
+                    </button>
+
+                    <p className="popup-with-form__switch-text">
+                        {switchText}{' '}
+                        <button className="popup-with-form__switch-link" type="button">
+                            {switchLinkText}
+                        </button>
+                    </p>
+                </form>
+            </div>
         </div>
     );
 }
