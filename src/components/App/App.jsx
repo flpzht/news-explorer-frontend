308f5e51ace4414e34b251bd2f5afd04cf30c08f
flpzht <flpcsb@hotmail.com>
2026-09-06 20:40:53 -0300

    fix: ajusta App conforme rota e status de login

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Header from '@/components/Header/Header';
 import Main from '@/components/Main/Main';
+import SavedNewsHeader from '@/components/SavedNewsHeader/SavedNewsHeader';
 import SavedNews from '@/components/SavedNews/SavedNews';
 import Footer from '@/components/Footer/Footer';
 import PopupWithForm from '@/components/PopupWithForm/PopupWithForm';
@@ -12,39 +13,54 @@ function App() {
 
   return (
     <div className="app">
-      <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/saved-news" element={<SavedNews />} />
+        <Route path="/" element={
+          <>
+            <Header />
+
+            <Main />
+
+            <PopupWithForm
+              title="Entrar"
+              buttonText="Entrar"
+              switchText="ou"
+              switchLinkText="Inscreva-se"
+            >
+              <label className="popup-with-form__field">
+                Email
+                <input
+                  className="popup-with-form__input"
+                  type="email"
+                  name="email"
+                  placeholder="Insira e-mail"
+                />
+              </label>
+
+              <label className="popup-with-form__field">
+                Senha
+                <input
+                  className="popup-with-form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Insira a senha"
+                />
+              </label>
+            </PopupWithForm>
+
+          </>}
+        />
+
+
+        <Route path="/saved-news" element={
+          <>
+            <SavedNewsHeader />
+            <SavedNews />
+          </>}
+        />
       </Routes>
+
       <Footer />
 
-      <PopupWithForm
-        title="Entrar"
-        buttonText="Entrar"
-        switchText="ou"
-        switchLinkText="Inscreva-se"
-      >
-        <label className="popup-with-form__field">
-          Email
-          <input
-            className="popup-with-form__input"
-            type="email"
-            name="email"
-            placeholder="Insira e-mail"
-          />
-        </label>
-
-        <label className="popup-with-form__field">
-          Senha
-          <input
-            className="popup-with-form__input"
-            type="password"
-            name="password"
-            placeholder="Insira a senha"
-          />
-        </label>
-      </PopupWithForm>
     </div>
   )
 }
