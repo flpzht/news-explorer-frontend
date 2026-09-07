fddbfa112f37b9b62d721076bfa4296855032a08
flpzht <flpcsb@hotmail.com>
2026-09-06 21:10:47 -0300

    feat: adiciona marcação do SavedNews e SavedNewsHeader

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,8 +1,16 @@
+import NewsCardList from '@/components/NewsCardList/NewsCardList';
+
 import '@/components/SavedNews/SavedNews.css';
 
 function SavedNews() {
     return (
         <main className="saved-news">
+            <section className="saved-news__hero">
+                <p className="saved-news__eyebrow">Artigos salvos</p>
+                <h1 className="saved-news__title">Elise, você tem 5 artigos salvos</h1>
+                <p className="saved-news__keywords">Por palavras-chaves: Natureza, Yellowstone, e outras 2</p>
+            </section>
+            <NewsCardList isSavedNewsPage />
         </main>
     );
 }
--- src/components/SavedNewsHeader/SavedNewsHeader.jsx
+++ src/components/SavedNewsHeader/SavedNewsHeader.jsx
@@ -1,8 +1,12 @@
+import Navigation from '@/components/Navigation/Navigation';
+
 import '@/components/SavedNewsHeader/SavedNewsHeader.css';
 
 function SavedNewsHeader() {
     return (
-        <section className="saved-news-header">    
+        <section className="saved-news-header">
+            <p className="saved-news-header__logo">News Explorer</p>
+            <Navigation />
         </section>
     );
 }
