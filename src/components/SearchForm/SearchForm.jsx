f2d943bf122e0a035b389652f5157aba38df76ce
flpzht <flpcsb@hotmail.com>
2026-09-09 16:20:50 -0300

    feat: estilização do SearchForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,13 +3,16 @@ import '@/components/SearchForm/SearchForm.css';
 function SearchForm() {
     return (
         <section className="search-form">
-            <h1 className="search-form__title">O que está acontecendo no mundo?</h1>
-            <p className="search-form__subtitle">Encontre as últimas notícias sobre qualquer tema e salve elas em sua conta pessoal</p>
+            <div className="search-form__container">
+                <h1 className="search-form__title">O que está 
+                    <br />acontecendo no mundo?</h1>
+                <p className="search-form__subtitle">Encontre as últimas notícias sobre qualquer tema e salve elas em sua conta pessoal</p>
 
-            <form action="" className="search-form__form" name="search-form" noValidate>
-                <input type="text" className="search-form__input" placeholder="Inserir tema" name="search" required />
-                <button type="submit" className="search-form__submit">Procurar</button>
-            </form>
+                <form action="" className="search-form__form" name="search-form" noValidate>
+                    <input type="text" className="search-form__input" placeholder="Inserir tema" name="search" required />
+                    <button type="submit" className="search-form__submit">Procurar</button>
+                </form>
+                </div>
         </section>
     );
 }
