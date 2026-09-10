759a9bfbccfa98784175933a73a81bc8571f7a85
flpzht <flpcsb@hotmail.com>
2026-09-10 12:54:58 -0300

    style: estiliza NewsCardList com background, hover, grid

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -8,26 +8,26 @@ import '@/components/NewsCardList/NewsCardList.css';
 const news = [
     {
     id: 1,
-    source: 'BBC News',
-    title: 'Título de exemplo da notícia número um',
-    description: 'Descrição breve de exemplo explicando do que se trata essa notícia fictícia.',
+    source: 'treehugger',
+    title: 'Todo mundo precisa de um "Lugar Especial para Sentar" especial na naturezaza',
+    description: 'Desde que li o influente livro de Richard Louv, "O Último Filho na Floresta", a ideia de ter um "lugar para sentar" especial me pegou de jeito. This advice, which Louv attributes to natureza...',
     publishedAt: '4 de novembro de 2020',
     urlToImage: newsCardImage01,
   },
   {
     id: 2,
-    source: 'CNN',
-    title: 'Título de exemplo da notícia número dois',
-    description: 'Outra descrição de exemplo para preencher o card durante a marcação.',
-    publishedAt: '19 de fevereiro de 2021',
+    source: 'national geographic',
+    title: 'A naturezaza faz de você uma pessoa melhor',
+    description: 'Todos nós sabemos como a natureza nos faz bem. Nós a conhecemos há milênios: o som dos oceanos, os aromas de uma floresta, a forma como a luz do sol dança através das folhas.',
+    publishedAt: '19 de fevereiro de 2019',
     urlToImage: newsCardImage01,
   },
   {
     id: 3,
-    source: 'Reuters',
-    title: 'Título de exemplo da notícia número três',
-    description: 'Mais um texto fictício para testar o layout em grade de três colunas.',
-    publishedAt: '25 de agosto de 2026',
+    source: 'National parques traveler',
+    title: 'Grand Teton renova a histórica Crest Trail',
+    description: '"A ligação entre as trilhas de Cascade e Death Canyon aconteceu em 1º de outubro de 1933, e marcou o primeiro passo na realização de um plano onde o viajante será...',
+    publishedAt: '19 de outubro de 2020',
     urlToImage: newsCardImage01,
   },
 ];
@@ -48,6 +48,8 @@ function NewsCardList({ isSavedNewsPage }) {
 
     return (
         <section className="news-card-list">
+            <div className="news-card-list__container">
+            {location.pathname === '/' ? <h2 className="news-card-list__title">Procurar resultados</h2> : ''}
             <ul className="news-card-list__items">
                 {news.map((article) => (
                     <li key={article.id} className="news-card-list__item">
@@ -55,8 +57,9 @@ function NewsCardList({ isSavedNewsPage }) {
                     </li>
                 ))}
             </ul>
-
+</div>
             <button className="news-card-list__button" type="button">Mostrar mais</button>
+            
         </section>
     );
 }
