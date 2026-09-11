6b5c174a6332dfcc5f68e15b9ff2623a7ae432ef
flpzht <flpcsb@hotmail.com>
2026-09-11 18:51:20 -0300

    feat: implementa estados login, registro e sucesso no PopupWithForm

--- src/components/PopupWithForm/PopupWithForm.jsx
+++ src/components/PopupWithForm/PopupWithForm.jsx
@@ -1,34 +1,64 @@
+import { useState } from 'react';
+
 import '@/components/PopupWithForm/PopupWithForm.css';
 
-function PopupWithForm({ title, buttonText, switchText, switchLinkText, children }) {
+function PopupWithForm({ isOpen, onClose }) {
+    const [mode, setMode] = useState('login');
+
+    function handleClose() {
+        setMode('login');
+        onClose();
+    }
+
+    function handleSwitchMode() {
+        setMode(mode === 'login' ? 'register' : 'login');
+    }
+
+    if (!isOpen) return null;
+
     return (
         <div className="popup-with-form">
-            <div className="popup-with-form__container">
-                <button
-                    className="popup-with-form__close-button"
-                    type="button"
-                    aria-label="Fechar"
-                >
-                </button>
+            <div className="popup-with-form__overlay">
+                <div className="popup-with-form__container">
+                    <button className="popup-with-form__close-button" type="button" aria-label="Fechar" onClick={handleClose} />
 
-                <h2 className="popup-with-form__title">{title}</h2>
+                    {mode === 'success' ? (
+                        <>
+                            <h2 className="popup-with-form__title">Cadastro concluído com sucesso!</h2>
+                            <button className="popup-with-form__switch-link popup-with-form__switch-link_type_success" type='button' onClick={() => setMode('login')}>Entrar</button>
+                        </>
+                    ) : (
+                        <>
+                            <h2 className="popup-with-form__title">{mode === 'login' ? 'Entrar' : 'Inscrever-se'}</h2>
 
-                <form className="popup-with-form__form" noValidate>
-                    {children}
+                            <form action="" className="popup-with-form__form" name="popup-with-form">
 
-                    <button className="popup-with-form__submit" type="submit">
-                        {buttonText}
-                    </button>
+                                <label htmlFor="" className="popup-with-form__field"> E-mail
+                                    <input className="popup-with-form__input" type="email" name='email' placeholder="Insira e-mail" required />
+                                </label>
 
-                    <p className="popup-with-form__switch-text">
-                        {switchText}{' '}
-                        <button className="popup-with-form__switch-link" type="button">
-                            {switchLinkText}
-                        </button>
-                    </p>
-                </form>
+                                <label htmlFor="" className="popup-with-form__field"> Senha
+                                    <input className="popup-with-form__input" type="password" name='password' placeholder="Insira a senha" required />
+                                </label>
+                                {mode === 'register' && (
+                                  <label htmlFor="" className="popup-with-form__field"> Nome de usuário
+                                    <input className="popup-with-form__input" type="text" name='usuario' placeholder="Insia seu nome de usuário" required />
+                                </label>  
+                                )}                                
+
+                                <button className="popup-with-form__submit" type="submit">{mode === 'login' ? 'Entrar' : 'Inscrever-se'}</button>
+
+                                <p className="popup-with-form__text">
+                                    ou{' '}
+                                    <button className="popup-with-form__switch-link" type="button" onClick={handleSwitchMode}>{mode === 'login' ? 'Inscrever-se' : 'Entrar'}</button>
+                                </p>
+
+                            </form>
+                        </>
+                    )}
+                </div>
             </div>
-        </div>
+        </div >
     );
 }
 
