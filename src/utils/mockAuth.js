948da0f08cff7ecaa4c242f3c3c8db2d52e54fde
flpzht <flpcsb@hotmail.com>
2026-09-11 18:48:42 -0300

    chore: remove isPopupOpen do mockAuth após implementar estado real no App

--- src/utils/mockAuth.js
+++ src/utils/mockAuth.js
@@ -1,4 +1,4 @@
 // TODO: arquivo temporário de simulação. Remover na Fase 3, quando existir Context real de autenticação.
-export const isLoggedIn = true;
+export const isLoggedIn = false;
 
 export const hasNoResults = false; // Simulação de ausência de resultados
