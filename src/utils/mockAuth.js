fed1bed5ebc541f3081dacf12cb9abbdcd3f40c2
flpzht <flpcsb@hotmail.com>
2026-09-12 00:08:03 -0300

    chore: remove hasNoResults do mockAuth, substituído por searchStatus

--- src/utils/mockAuth.js
+++ src/utils/mockAuth.js
@@ -1,4 +1,2 @@
 // TODO: arquivo temporário de simulação. Remover na Fase 3, quando existir Context real de autenticação.
 export const isLoggedIn = false;
-
-export const hasNoResults = false; // Simulação de ausência de resultados
