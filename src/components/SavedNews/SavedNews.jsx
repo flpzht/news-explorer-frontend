2aa868dfc658ec313baff92512fdc56b1f96805c
flpzht <flpcsb@hotmail.com>
2026-09-11 22:46:05 -0300

    style: estliza SavedNews_Hero fonte, margin, background

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -7,8 +7,8 @@ function SavedNews() {
         <main className="saved-news">
             <section className="saved-news__hero">
                 <p className="saved-news__eyebrow">Artigos salvos</p>
-                <h1 className="saved-news__title">Elise, você tem 5 artigos salvos</h1>
-                <p className="saved-news__keywords">Por palavras-chaves: Natureza, Yellowstone, e outras 2</p>
+                <h1 className="saved-news__title">Elise, você tem 3 artigos salvos</h1>
+                <p className="saved-news__keywords">Por palavras-chaves: Natureza, Yellowstone, e 2 outras</p>
             </section>
             <NewsCardList isSavedNewsPage />
         </main>
