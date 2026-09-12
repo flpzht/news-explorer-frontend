8b5769271cf5f57380155900ef989252a84b7bf8
flpzht <flpcsb@hotmail.com>
2026-09-12 00:29:09 -0300

    style: adiciona texto e estilo ao Preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -4,6 +4,7 @@ function Preloader() {
   return (
     <div className="preloader">
     <i className="circle-preloader"></i>
+    <p className="preloader__text">Procurando notícias...</p>
     </div>
   );
 }
