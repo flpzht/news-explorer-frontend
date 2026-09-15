968e93c7af3099859cea252a539cf2d046e62ccf
flpzht <flpcsb@hotmail.com>
2026-09-14 21:22:51 -0300

    refactor: criacao container para ajustar dimensoes no About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,17 +5,19 @@ import '@/components/About/About.css';
 function About() {
     return (
         <section className="about">
-            <img className="about__photo" src={aboutPhoto} alt="Foto de uma pessoa lendo notícias" />
-            <div className="about__content">
-                <h2 className="about__title">Sobre o autor</h2>
-                <p className="about__text">
-                    Esse bloco descreve o autor do projeto. Aqui você deve indicar seu nome,
-                    o que você faz e quais tecnologias de desenvolvedor você conhece.
-                </p>
-                <p className="about__text">
-                    Você também pode falar sobre sua experiência com o Practicum, o que
-                    aprendeu lá e como pode ajudar clientes em potencial.
-                </p>
+            <div className="about__container">
+                <img className="about__photo" src={aboutPhoto} alt="Foto de uma pessoa lendo notícias" />
+                <div className="about__content">
+                    <h2 className="about__title">Sobre o autor</h2>
+                    <p className="about__text">
+                        Esse bloco descreve o autor do projeto. Aqui você deve indicar seu nome,
+                        o que você faz e quais tecnologias de desenvolvedor você conhece.
+                    </p>
+                    <p className="about__text">
+                        Você também pode falar sobre sua experiência com o Practicum, o que
+                        aprendeu lá e como pode ajudar clientes em potencial.
+                    </p>
+                </div>
             </div>
         </section>
     );
