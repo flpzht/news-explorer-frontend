946f22f39f47c95f291fb15358366ea891400c84
flpzht <flpcsb@hotmail.com>
2026-09-18 13:12:41 -0300

    feat: adapta NewsCardList para consumir articles reais da API

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -2,39 +2,12 @@ import { useLocation } from 'react-router-dom';
 
 import NewsCard from '@/components/NewsCard/NewsCard';
 
-import newsCardImage01 from '@/images/news-card-image01.png';
 import notFoundIcon from '@/images/not-found-image.svg';
 
 import '@/components/NewsCardList/NewsCardList.css';
 
-const news = [
-    {
-        id: 1,
-        source: 'treehugger',
-        title: 'Todo mundo precisa de um "Lugar Especial para Sentar" especial na natureza',
-        description: 'Desde que li o influente livro de Richard Louv, "O Último Filho na Floresta", a ideia de ter um "lugar para sentar" especial me pegou de jeito. This advice, which Louv attributes to natureza...',
-        publishedAt: '4 de novembro de 2020',
-        urlToImage: newsCardImage01,
-    },
-    {
-        id: 2,
-        source: 'national geographic',
-        title: 'A naturezaza faz de você uma pessoa melhor',
-        description: 'Todos nós sabemos como a natureza nos faz bem. Nós a conhecemos há milênios: o som dos oceanos, os aromas de uma floresta, a forma como a luz do sol dança através das folhas.',
-        publishedAt: '19 de fevereiro de 2019',
-        urlToImage: newsCardImage01,
-    },
-    {
-        id: 3,
-        source: 'National parques traveler',
-        title: 'Grand Teton renova a histórica Crest Trail',
-        description: '"A ligação entre as trilhas de Cascade e Death Canyon aconteceu em 1º de outubro de 1933, e marcou o primeiro passo na realização de um plano onde o viajante será...',
-        publishedAt: '19 de outubro de 2020',
-        urlToImage: newsCardImage01,
-    },
-];
 
-function NewsCardList({ isSavedNewsPage, searchStatus }) {
+function NewsCardList({ isSavedNewsPage, searchStatus, articles }) {
     const location = useLocation();
 
     if (searchStatus === 'empty') {
@@ -52,8 +25,8 @@ function NewsCardList({ isSavedNewsPage, searchStatus }) {
             <div className="news-card-list__container">
                 {location.pathname === '/' ? <h2 className="news-card-list__title">Procurar resultados</h2> : ''}
                 <ul className="news-card-list__items">
-                    {news.map((article) => (
-                        <li key={article.id} className="news-card-list__item">
+                    {articles.map((article, index) => (
+                        <li key={`${article.id}-${index}`}className="news-card-list__item">
                             <NewsCard card={article} isSavedNewsPage={isSavedNewsPage} />
                         </li>
                     ))}
