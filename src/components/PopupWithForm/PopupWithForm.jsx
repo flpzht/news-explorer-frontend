e068788197e1aa4be3539259ad1df9544f52db7b
flpzht <flpcsb@hotmail.com>
2026-09-18 19:07:38 -0300

    feat: adiciona validação de formulário controlando estado disabled do botão

--- src/components/PopupWithForm/PopupWithForm.jsx
+++ src/components/PopupWithForm/PopupWithForm.jsx
@@ -4,15 +4,30 @@ import '@/components/PopupWithForm/PopupWithForm.css';
 
 function PopupWithForm({ isOpen, onClose }) {
     const [mode, setMode] = useState('login');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [userName, setUserName] = useState('');
 
     function handleClose() {
         setMode('login');
+        setEmail('');
+        setPassword('');
+        setUserName('');
         onClose();
     }
 
     function handleSwitchMode() {
         setMode(mode === 'login' ? 'register' : 'login');
+        setEmail('');
+        setPassword('');
+        setUserName('');
     }
+
+    const isEmailValid = email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+    const isPasswordValid = password.length >= 8;
+    const isUserNameValid = userName.length >= 2;
+
+    const isFormValid = mode === 'login' ? isEmailValid && isPasswordValid : isEmailValid && isPasswordValid && isUserNameValid;
 
     if (!isOpen) return null;
 
@@ -31,22 +46,22 @@ function PopupWithForm({ isOpen, onClose }) {
                         <>
                             <h2 className="popup-with-form__title">{mode === 'login' ? 'Entrar' : 'Inscrever-se'}</h2>
 
-                            <form action="" className="popup-with-form__form" name="popup-with-form">
+                            <form action="" className="popup-with-form__form" name="popup-with-form"> {/*Fase 3: adicionar onSubmit real (login/registro via API) no <form> abaixo */}
 
-                                <label htmlFor="" className="popup-with-form__field"> E-mail
-                                    <input className="popup-with-form__input" type="email" name='email' placeholder="Insira e-mail" required />
+                                <label className="popup-with-form__field"> E-mail
+                                    <input className="popup-with-form__input" type="email" name='email' placeholder="Insira e-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
                                 </label>
 
-                                <label htmlFor="" className="popup-with-form__field"> Senha
-                                    <input className="popup-with-form__input" type="password" name='password' placeholder="Insira a senha" required />
+                                <label className="popup-with-form__field"> Senha
+                                    <input className="popup-with-form__input" type="password" name='password' placeholder="Insira a senha" value={password} onChange={(e) => setPassword(e.target.value)} required />
                                 </label>
                                 {mode === 'register' && (
-                                  <label htmlFor="" className="popup-with-form__field"> Nome de usuário
-                                    <input className="popup-with-form__input" type="text" name='usuario' placeholder="Insia seu nome de usuário" required />
+                                  <label className="popup-with-form__field"> Nome de usuário
+                                    <input className="popup-with-form__input" type="text" name='usuario' placeholder="Insia seu nome de usuário"  value={userName} onChange={(e) => setUserName(e.target.value)} required />
                                 </label>  
                                 )}                                
 
-                                <button className="popup-with-form__submit" type="submit">{mode === 'login' ? 'Entrar' : 'Inscrever-se'}</button>
+                                <button className="popup-with-form__submit" type="submit" disabled={!isFormValid}>{mode === 'login' ? 'Entrar' : 'Inscrever-se'}</button>
 
                                 <p className="popup-with-form__text">
                                     ou{' '}
